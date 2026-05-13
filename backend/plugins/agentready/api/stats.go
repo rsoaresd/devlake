be72db486dc691d21cad642ddc43858c61f80b7d
Bryan Ramos <bramos@redhat.com>
2026-05-13 14:33:34 -0400

    feat: improve stats sql
    Do the math DB-side rather than in Go.
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- backend/plugins/agentready/api/stats.go
+++ backend/plugins/agentready/api/stats.go
@@ -9,45 +9,65 @@ import (
 	"github.com/apache/incubator-devlake/plugins/agentready/models"
 )
 
+type AggResult struct {
+	Total    int64   `gorm:"column:total"`
+	AvgScore float64 `gorm:"column:avg_score"`
+}
+
+type CertCount struct {
+	CertificationLevel string `gorm:"column:certification_level" json:"certificationLevel"`
+	Count              int64  `gorm:"column:count" json:"count"`
+}
+
 func GetStats(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	var clauses []dal.Clause
+	var baseClauses []dal.Clause
 
 	if projectName := input.Query.Get("projectName"); projectName != "" {
-		clauses = []dal.Clause{
-			dal.Select("a.*"),
+		baseClauses = []dal.Clause{
 			dal.From("_tool_agentready_assessments a"),
 			dal.Join("JOIN project_mapping pm ON a.repo_id = pm.row_id"),
 			dal.Where("pm.project_name = ? AND pm.`table` = ?", projectName, "repos"),
 		}
 	} else {
-		clauses = []dal.Clause{
+		baseClauses = []dal.Clause{
 			dal.From(&models.AgentReadyAssessment{}),
 		}
 	}
-	clauses = append(clauses, dal.Where("id != ''"))
+	baseClauses = append(baseClauses, dal.Where("id != ''"))
 
-	var assessments []models.AgentReadyAssessment
-	err := db.All(&assessments, clauses...)
+	aggClauses := make([]dal.Clause, len(baseClauses))
+	copy(aggClauses, baseClauses)
+	aggClauses = append(aggClauses,
+		dal.Select("COUNT(*) as total, AVG(overall_score) as avg_score"),
+	)
+
+	var agg AggResult
+	err := db.First(&agg, aggClauses...)
 	if err != nil {
-		return nil, errors.Default.Wrap(err, "failed to query assessments for stats")
+		return nil, errors.Default.Wrap(err, "failed to get assessment stats")
 	}
 
-	certDist := map[string]int{}
-	var totalScore float64
-	for _, a := range assessments {
-		certDist[a.CertificationLevel]++
-		totalScore += a.OverallScore
+	var certCounts []CertCount
+	certClauses := make([]dal.Clause, len(baseClauses))
+	copy(certClauses, baseClauses)
+	certClauses = append(certClauses,
+		dal.Select("certification_level, COUNT(*) as count"),
+		dal.Groupby("certification_level"),
+	)
+	err = db.All(&certCounts, certClauses...)
+	if err != nil {
+		return nil, errors.Default.Wrap(err, "failed to get certification distribution")
 	}
 
-	avgScore := 0.0
-	if len(assessments) > 0 {
-		avgScore = totalScore / float64(len(assessments))
+	certDist := make(map[string]int, len(certCounts))
+	for _, c := range certCounts {
+		certDist[c.CertificationLevel] = int(c.Count)
 	}
 
 	return &plugin.ApiResourceOutput{
 		Body: map[string]interface{}{
-			"totalRepos":                len(assessments),
-			"averageScore":              avgScore,
+			"totalRepos":                agg.Total,
+			"averageScore":              agg.AvgScore,
 			"certificationDistribution": certDist,
 		},
 		Status: http.StatusOK,
